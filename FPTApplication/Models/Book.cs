c5a9e6b8eb2522490466f4b91b3eaec2da68f307
vothilena123 <navtlgcs190745@fpt.edu.vn>
2022-05-10 08:19:56 +0700

    fpt

---
+++ FPTApplication/Models/Book.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Web;
+
+namespace FPTApplication.Models
+{
+    public class Book
+    {
+        public Book()
+        {
+            ImagePath = "~/Publish/Images/OIP.jfif";
+        }
+
+        [Key]
+        public Guid BookID { get; set; }
+
+        [Required]
+        public string BookName { get; set; }
+
+        [Required]
+        public string ImagePath { get; set; }
+
+        [Required]
+        public decimal BookPrice { get; set; }
+
+        [NotMapped]
+        public HttpPostedFileBase ImageUpload { get; set; }
+    }
+}
---
+++ FPTApplication/Models/FPTDBContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+
+namespace FPTApplication.Models
+{
+    public class FPTDBContext : IdentityDbContext<ApplicationUser>
+    {
+        public virtual DbSet<Book> Books { get; set; }
+        
+        public FPTDBContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static FPTDBContext Create()
+        {
+            return new FPTDBContext();
+        }
+    }
+}
